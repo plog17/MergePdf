358ace6ef34d118df954094d577a078c78f27af4
Pier-Luc Auger <auger.pierluc@gmail.com>
2015-03-12 11:32:20 -0400

    Merge pdf

---
+++ mergePdf.py
@@ -0,0 +1,22 @@
+import sys
+import os
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+pathToPic='pic/front/'
+pathToOut='pic/out/'
+back = PdfFileReader(open("pic/back/Back.pdf", "rb"))
+output = PdfFileWriter()
+
+for filename in os.listdir('pic/front/'):
+     if filename != ".DS_Store":
+         input1 = PdfFileReader(open(pathToPic+filename, "rb"))
+         output.addPage(back.getPage(0))
+         output.addPage(input1.getPage(0))
+         outputStreamSingle = file(pathToOut+"single_"+filename, "wb")
+         output.write(outputStreamSingle)
+
+
+outputStream = file(pathToOut+"total_"+filename, "wb")
+output.write(outputStream)
+
+print("HELLO");
